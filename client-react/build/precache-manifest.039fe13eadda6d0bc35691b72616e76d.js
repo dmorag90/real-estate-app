f90fb57ceaf176b4e8fa9cee2cd5f73b8be4086f
Daniel Morag <dmorag90@gmail.com>
2021-01-19 09:59:04 +0200

    add the build to the folder

---
+++ client-react/build/precache-manifest.039fe13eadda6d0bc35691b72616e76d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4076f6e720aed55d27884e5046a1653b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373b1e2fc0f93bf7a58f",
+    "url": "/static/css/2.302ba763.chunk.css"
+  },
+  {
+    "revision": "138d0da13eb5efdce7df",
+    "url": "/static/css/main.9dfacdfc.chunk.css"
+  },
+  {
+    "revision": "373b1e2fc0f93bf7a58f",
+    "url": "/static/js/2.716f4f2a.chunk.js"
+  },
+  {
+    "revision": "abf0c69dd451c2e0d1efd7145e96f9c5",
+    "url": "/static/js/2.716f4f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138d0da13eb5efdce7df",
+    "url": "/static/js/main.4126448b.chunk.js"
+  },
+  {
+    "revision": "e60a10bd98deddfdda45",
+    "url": "/static/js/runtime-main.3038e589.js"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "263503bd1e083b614bef036a32f97e83",
+    "url": "/static/media/house.263503bd.jpg"
+  },
+  {
+    "revision": "d8c3e6e14b5e521cfc68343638b012b6",
+    "url": "/static/media/portrait.d8c3e6e1.png"
+  },
+  {
+    "revision": "247f793dd09925ce0bf9494bd7c09378",
+    "url": "/static/media/road.247f793d.jpg"
+  }
+]);
